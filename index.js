8e7b7731048b5f411f98ab3a19d09efeff119f5a
thaitran <43898236+thaitranxyz@users.noreply.github.com>
2022-12-21 00:21:23 +1100

    Add note part

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ const container = document.querySelector(".container");
 const fireworks = new Fireworks(container, {});
 
 fireworks.start();
+
+const noteBtn = document.querySelector(".note-button");
+const note = document.querySelector(".note");
+
+noteBtn.addEventListener("click", () => {
+  noteBtn.classList.add("hidden");
+  note.classList.remove("hidden");
+});
